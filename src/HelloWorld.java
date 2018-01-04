b8f4b5b19bc32d06d88a9287046d7ab4ad70101b
Mads Østergaard <m@madsostergaard.dk>
2018-01-04 16:57:34 +0100

    testing git

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class HelloWorld {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		System.out.println("fuk u");
+		System.out.println("tell me stuff:");
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		bf.readLine();
+		System.out.println("lol");
 	}
 
 }
