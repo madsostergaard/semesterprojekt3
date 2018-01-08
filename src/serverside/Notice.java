5ad51a5b822dfc895bfad68b3d6888e7f9958e62
Dorthe <Dorthe@DESKTOP-5J20KON>
2018-01-08 14:16:52 +0100

    Notice sjov

--- src/serverside/Notice.java
+++ src/serverside/Notice.java
@@ -11,57 +11,55 @@ public class Notice {
 	private Date date; // Gemmes det som et Date objekt i databasen eller er det bare en string?
 	private DateFormat format = new SimpleDateFormat("mmddyy"); // Skal vi have tiden med, eller ny attribut?
 	private String time;
-	private String title; 
+	private String title;
 	private String URL;
-	
+	private int hospID;
 
-	Notice(String title, String URL, String date ) throws ParseException{
+	Notice(String title, String URL, String date, int hospID) throws ParseException {
 		this.date = format.parse(date);
 		this.title = title;
 		this.URL = URL;
+		this.hospID = hospID;
 	}
-
 
 	public Date getDate() {
 		return date;
 	}
 
-
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
+
 	public String getTime() {
 		return time;
 	}
-
 
 	public void setTime(String time) {
 		this.time = time;
 	}
 
-
 	public String getTitle() {
 		return title;
 	}
-
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-
 	public String getURL() {
 		return URL;
 	}
 
-
 	public void setURL(String URL) {
 		this.URL = URL;
 	}
-	
-	
-	
-	
-	
+
+	public int getUuid() {
+		return hospID;
+	}
+
+	public void setUuid(int hospID) {
+		this.hospID = hospID;
+	}
+
 }
--- src/serverside/Notices.java
+++ src/serverside/Notices.java
@@ -1,5 +1,44 @@
 package serverside;
 
+import java.util.ArrayList;
+
 public class Notices {
+
+	private int uuid;
+	private ArrayList<Notice> notice;
+
+	Notices(int uuid) {
+
+		this.uuid = uuid;
+	}
+
+	Notices(int uuid, ArrayList<Notice> notice) {
+
+		this.uuid = uuid;
+		this.notice = notice;
+
+	}
+
+	public int getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(int uuid) {
+		this.uuid = uuid;
+	}
+
+	public ArrayList<Notice> getNotice() {
+		return notice;
+	}
+
+	public void setNotice(ArrayList<Notice> notice) {
+		this.notice = notice;
+	}
+
+	public void addNotice(Notice notice) {
+
+		this.notice.add(notice);
+
+	}
 
 }
